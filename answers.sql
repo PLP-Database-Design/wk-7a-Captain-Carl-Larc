e4c4293cccf96d382480ba67a96a772a8e413dac
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-08 10:39:50 +0300

    question2 done

--- answers.sql
+++ answers.sql
@@ -4,3 +4,17 @@ CREATE TABLE ProductDetail_1NF (
     CustomerName VARCHAR(255),
     Product VARCHAR(255)
 );
+
+-- Create the Orders table
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(255)
+);
+
+-- Create the OrderItems table
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(255),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product)
+);
